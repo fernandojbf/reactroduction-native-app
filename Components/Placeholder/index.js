2a8132c3180079330218139d6002d25d5157ad44
Fernando Ferreira <fernando.ferreira@mindera.com>
2017-09-09 13:39:21 +0100

    Fix typo

--- Components/Placeholder/index.js
+++ Components/Placeholder/index.js
@@ -4,7 +4,7 @@ import {StyleSheet, View, Text, Image } from 'react-native';
 const Placeholder = () =>
   <View style={styles.container}>
     <Image style={{height: 100, width: 100, marginBottom: 20}} source={require('./beer.png')} />
-    <Text>You are free to do whatever you whant.</Text>
+    <Text>You are free to do whatever you want.</Text>
   </View>;
 
 export default Placeholder;
